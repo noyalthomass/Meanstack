45260e6136affa57c48cadca2d28aff1996f4dc6
Noyal Thomas <noyallthomas@gmail.com>
2021-09-24 12:44:13 +0530

    meanstack

---
+++ Meanstack/array_prgrms/qstns.js
@@ -0,0 +1,36 @@
+//day1
+var expenses=[12000,13000,14000,15000,11000,25000,17000]
+//print highest exp
+//print lowest exp
+//print etra monthil above 15000 ayittnd
+//print avg exp
+
+var arr=[2,4,6,3]   //[13,11,9,12]
+var arr=[1,2,3,4]  //sum=6 {2,4}  sum=5 {2,3}{1,4}   sum=7 {3,4} 
+
+//day2
+
+//arr1=[10,20,21,22]
+//arr2=[11,12,20,21]
+//print common elem [20,21]
+
+//arr=[10,11,12,13,14,11] search for 15 in this array ele fount or not fount
+
+//arr=[10,11,12,11,10] find duplicate element 10,11
+
+//day3
+//gcd 0f (14,21)
+
+//day4
+var products=[
+    [100,"oreo",40,50],
+    [101,"fifty-fifty",20,60],
+    [102,"borbourne",40,0],
+    [103,"treat",30,10],
+    [104,"arrowroot",30,0],
+    [105,"moms-magic",45,10]
+    ]
+
+    //print number of items in shop
+    //print number of available items
+    //print all product names only
